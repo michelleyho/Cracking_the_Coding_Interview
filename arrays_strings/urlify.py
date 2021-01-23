ea368b62438c86eb43e48d7ac19f1f33d19a5708
Michelle Y Ho <michelle.y.ho@gmail.com>
2021-01-23 14:02:09 -0800

    init version

---
+++ arrays_strings/urlify.py
@@ -0,0 +1,37 @@
+'''
+Write a method to replace all spaces in a string with "%20".  You may assume that the string has sufficient space at teh end to hold the additional characters, and that you are given the "True" length of the string."
+'''
+
+# TODO: more testcases
+
+'''
+Time: O(n)
+Space: O(n)
+    - Python strings are immutable...
+      Need to create a new array to modify string
+'''
+def urlify(phrase, length):
+    new_phrase = [None]*len(phrase)
+    last_char = phrase[length-1]
+    
+    right_ptr = len(phrase)-1
+    left_ptr = length-1
+
+    while left_ptr >= 0:
+        curr = phrase[left_ptr]
+        if curr != ' ':
+            new_phrase[right_ptr] = curr
+            right_ptr -= 1
+        else:
+            new_phrase[right_ptr-2:right_ptr+1] = '%20' 
+            right_ptr -= 3
+
+        left_ptr -= 1
+    print new_phrase
+    return ''.join(new_phrase)
+
+
+if __name__ == "__main__":
+    p, l = "Mr John Smith    ", 13
+    print urlify(p, l)
+
